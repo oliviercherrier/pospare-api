788b649bf52f8d8922e791b24aa249c5aa57aa59
oliviercherrier <olivier.cherrier@gmail.com>
2017-07-02 22:31:46 +0200

    Put Workout for a user

--- routes/workout.js
+++ routes/workout.js
@@ -8,7 +8,26 @@ var User = require('../models/User.js');
 // /users GET
 // /users/:userId GET
 
-/* GET users listing. */
+/* PUT workout for user req.header.useremail */
+workoutRouter.put('/', function(req, res, next) {
+  User.findByPospareId(req.params.userId, ["workouts"], function(err, user) {
+    if (err) res.send(err);
+    if(!user){
+      res.sendStatus(204);
+    }
+    else{
+      // Add workout for this user
+      var newWorkout = new workout();
+      newWorkout.save();
+      user.workouts.push(newWorkout);
+      console.log(user);
+      user.save();
+      res.json({ message: 'Workout updated!' });
+    }
+  });
+});
+
+/* GET workouts listing for the user req.header.useremail */
 workoutRouter.get('/', function(req, res, next) {
   User.findByPospareId(req.params.userId, ["workouts"], function(err, user) {
     if (err) res.send(err);
